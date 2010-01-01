c84de7c9e5b8e9718dfa52dc86cdac677f02533c
Florian Ragwitz <rafl@debian.org>
2010-01-01 18:28:07 +0100

    moar stuff.

--- drum_rocker.c
+++ drum_rocker.c
@@ -5,6 +5,12 @@
 #include <linux/module.h>
 #include <linux/usb.h>
 
+#define DRUM_ROCKER_BUFSIZE 64
+
+struct drum_rocker {
+	char inbuf[DRUM_ROCKER_BUFSIZE];
+};
+
 static struct usb_device_id id_table[] = {
 	{ USB_DEVICE(0x12ba, 0x0210) },
 	{ }
@@ -12,28 +18,57 @@ static struct usb_device_id id_table[] = {
 
 MODULE_DEVICE_TABLE (usb, id_table);
 
+static void
+irq_in (struct urb *urb)
+{
+	struct drum_rocker *drum = urb->context;
+
+	switch (urb->status) {
+		case 0: /* success */
+			printk (KERN_INFO "foo %d\n", urb->actual_length);
+			usb_submit_urb (urb, GFP_ATOMIC);
+			break;
+		default:
+			dev_warn (&urb->dev->dev, "input irq status %d received\n", urb->status);
+	}
+
+}
+
 static int
 drum_rocker_probe (struct usb_interface *interface, const struct usb_device_id *id)
 {
-	dev_info (&interface->dev, "drum rocker device now attached\n");
+	struct usb_device *dev = interface_to_usbdev (interface);
+	int pipe = usb_rcvintpipe (dev, 1);
+	struct urb *urbin = usb_alloc_urb (0, GFP_KERNEL);
+	struct drum_rocker *drum;
+
+	drum = kmalloc (sizeof (struct drum_rocker), GFP_KERNEL);
+	memset (drum, 0x00, sizeof (*drum));
+	usb_set_intfdata (interface, drum);
+
+	dev_info (&interface->dev, "drum rocker device attached\n");
+
+	usb_fill_int_urb (urbin, dev, pipe, drum->inbuf, DRUM_ROCKER_BUFSIZE, irq_in, drum, 10);
+	usb_submit_urb (urbin, GFP_ATOMIC);
+
 	return 0;
 }
 
 static void
 drum_rocker_disconnect (struct usb_interface *interface)
 {
-	dev_info (&interface->dev, "drum rocker now disconnected\n");
+	dev_info (&interface->dev, "drum rocker disconnected\n");
 }
 
 static struct usb_driver drum_rocker_driver = {
-	.name       = "drumdocker",
+	.name       = "drum_rocker",
 	.probe      = drum_rocker_probe,
 	.disconnect = drum_rocker_disconnect,
 	.id_table   = id_table
 };
 
 static int
-__init drum_rocker_init ()
+__init drum_rocker_init (void)
 {
 	int ret = 0;
 
@@ -46,7 +81,7 @@ __init drum_rocker_init ()
 }
 
 static void
-__exit drum_rocker_exit ()
+__exit drum_rocker_exit (void)
 {
 	usb_deregister (&drum_rocker_driver);
 }
